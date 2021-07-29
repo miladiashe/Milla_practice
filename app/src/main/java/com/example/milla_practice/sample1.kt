7ce955aefd12d429214b29bd093de9aaa9bd7779
miladiashe <dkdud0122@naver.com>
2021-07-30 08:40:03 +0900

    함수공부

--- app/src/main/java/com/example/milla_practice/sample1.kt
+++ app/src/main/java/com/example/milla_practice/sample1.kt
@@ -2,6 +2,8 @@ package com.example.milla_practice
 
 fun main(){
     helloWorld()
+
+    println(add(17, 24))
 }
 
 
@@ -9,4 +11,8 @@ fun main(){
 
 fun helloWorld(){
     println("안녕세계야")
+}
+
+fun add(a : Int, b: Int) : Int{
+    return a+b
 }
